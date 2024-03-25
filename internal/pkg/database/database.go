193dc1971ea04d24c9683c40fe3ea20a733bed8e
Wanderson Paes <wandersonpaeswop3@gmail.com>
2024-03-25 10:30:45 -0300

    Initial database config

---
+++ internal/pkg/database/database.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+var databaseConnectionString = ""
+
+func SetUpDatabase() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	databaseConnectionString = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+}
+
+func Conectar() (*sql.DB, error) {
+	db, erro := sql.Open("mysql", databaseConnectionString)
+	if erro != nil {
+		return nil, erro
+	}
+
+	if erro = db.Ping(); erro != nil {
+		db.Close()
+		return nil, erro
+	}
+
+	return db, nil
+
+}
--- main.go
+++ main.go
@@ -6,9 +6,12 @@ import (
 	"net/http"
 
 	router "github.com/wandersonpaes/runners-api/internal/app"
+	"github.com/wandersonpaes/runners-api/internal/pkg/database"
 )
 
 func main() {
+	database.SetUpDatabase()
+
 	fmt.Println("Runners API is running!")
 	r := router.CreateMux()
 
